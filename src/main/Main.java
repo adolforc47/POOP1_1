8de669603d86b1b7502e15b1c4a4cf99cbf56df5
adolforc47 <111707538+adolforc47@users.noreply.github.com>
2023-10-12 23:40:20 -0600

    Creación de la Práctica 1 ejercicio 1

---
+++ src/main/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author Rojas Castañeda Adolfo
+ */
+public class Main {
+
+    /**
+     * @param 
+     * Imprime el mensaje "Hola mundo desde POO, grupo 8, desde Ant" en la consola.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo desde POO, grupo 8, desde Ant");
+    }
+    
+}
